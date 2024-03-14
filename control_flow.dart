78b5f816838d043dc45dce4a0b10498e8ce69ce1
ibrahim-kiri <ibrahimkiri@gmail.com>
2024-03-14 11:59:06 +0300

    control-flow assignment

---
+++ control_flow.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  print("Enter a number:");
+  String? input = stdin.readLineSync();
+  if (input != null) {
+    int number = int.tryParse(input) ?? 0;
+    if (number > 10) {
+      print("Your number is greater than 10");
+    } else if (number < 10) {
+      print("Your number is less than 10");
+    } else {
+      print("Your number is equal to 10");
+    }
+  }
+}
